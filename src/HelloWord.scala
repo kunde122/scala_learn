3f7f6f5646510d035db3672072348d1ecdf42b1c
曹振天 <caozhentian@yadingdata.com>
2019-08-08 10:37:46 +0800

    1

---
+++ src/HelloWord.scala
@@ -0,0 +1,5 @@
+class HelloWord {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
---
+++ src/hello.scala
@@ -0,0 +1,13 @@
+object hello1 {
+  /* 这是我的第一个 Scala 程序
+
+      * 以下程序将输出'Hello World!'
+
+      */
+
+  def main(args: Array[String]) {
+
+    println("Hello, world!gdhfhf") // 输出 Hello World
+
+  }
+}
